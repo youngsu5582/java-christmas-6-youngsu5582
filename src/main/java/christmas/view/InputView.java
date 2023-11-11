5d4a71e2a19f7881e2d2f147ab6324e006f07dea
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-11 14:50:01 +0900

    feat : 입출력 담당 하는 InputView,OutputView 추가
    입력 담당 하는 InputView
    출력 담당 하는 OutputView

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static String readInput(){
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,7 @@
+package christmas.view;
+
+public class OutputView {
+    private static void printMessage(String message){
+        System.out.println(message);
+    }
+}
