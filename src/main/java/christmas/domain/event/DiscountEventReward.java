d9bd891734b64d9eaa7bae9e05aafbfda5b55796
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-13 15:30:45 +0900

    feat : EventReward 추가
    공통 적으로 사용 위한 상위 클래스 EventReward 추가
    DiscountEventReward, PresentEventReward 들이 상속
    상속을 위해서 record -> class 로 변경

--- src/main/java/christmas/domain/event/DiscountEventReward.java
+++ src/main/java/christmas/domain/event/DiscountEventReward.java
@@ -1,4 +1,19 @@
 package christmas.domain.event;
 
-public record DiscountEventReward(String eventName, int discountPrice) {
+import christmas.domain.menu.Menu;
+import christmas.lib.event.EventReward;
+
+public class DiscountEventReward extends EventReward {
+    private final String eventName;
+    private final int discountPrice;
+    public DiscountEventReward(String eventName, int discountPrice){
+        this.eventName=eventName;
+        this.discountPrice=discountPrice;
+    }
+    public String eventName(){
+        return this.eventName;
+    }
+    public int discountPrice(){
+        return discountPrice;
+    }
 }
--- src/main/java/christmas/domain/event/PresentEventReward.java
+++ src/main/java/christmas/domain/event/PresentEventReward.java
@@ -1,6 +1,19 @@
 package christmas.domain.event;
 
 import christmas.domain.menu.Menu;
+import christmas.lib.event.EventReward;
 
-public record PresentEventReward(Menu present , int count) {
+public class PresentEventReward extends EventReward {
+    private final Menu present;
+    private final int count;
+    public PresentEventReward(Menu present,int count){
+        this.present=present;
+        this.count=count;
+    }
+    public Menu present(){
+        return this.present;
+    }
+    public int count(){
+        return count;
+    }
 }
