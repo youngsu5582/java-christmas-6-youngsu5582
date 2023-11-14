54276b0f8bfe7464a03d3ea858da2674ed037758
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-14 16:13:30 +0900

    feat : Date Service try - catch 문 추가
    Parser 중 에러 발생 시 , INVALID_DAY 발생 하게 변경

--- src/main/java/christmas/service/DateService.java
+++ src/main/java/christmas/service/DateService.java
@@ -1,12 +1,19 @@
 package christmas.service;
 
 import christmas.domain.Date;
+import christmas.exception.DateException;
+import christmas.exception.ParserException;
+import christmas.exception.message.DateExceptionMessage;
 import christmas.util.Parser;
 
 public class DateService {
-    public Date createDate(String dayInfo){
-            Integer day = Parser.parseInfoToNumber(dayInfo);
+    public Date createDate(String dayInfo) {
+        try {
+            int day = Parser.parseInfoToNumber(dayInfo);
             Date date = Date.of(day);
             return date;
+        }catch (ParserException exception){
+            throw new DateException(DateExceptionMessage.INVALID_DAY);
+        }
     }
 }
