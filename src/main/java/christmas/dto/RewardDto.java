d67a2c76e67dc5386855e0487ac1f9b625e6d1e2
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-11-14 16:12:43 +0900

    feat : RewardDto getTotalDiscountReward 추가
    Discount 를 통해 , 할인된 값만 모아서 반환하는
    getter 추가

--- src/main/java/christmas/dto/RewardDto.java
+++ src/main/java/christmas/dto/RewardDto.java
@@ -7,4 +7,11 @@ import java.util.List;
 
 public record RewardDto(int totalRewardPrice, List<DiscountEventReward> discountRewards,
                         List<PresentEventReward> presentRewards) {
+    public Integer getTotalDiscountReward() {
+        int totalDiscountReward = 0;
+        for (DiscountEventReward discountEventReward : discountRewards) {
+            totalDiscountReward += discountEventReward.discountPrice();
+        }
+        return totalDiscountReward;
+    }
 }
